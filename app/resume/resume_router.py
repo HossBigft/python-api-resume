3495be89e1ba13c2424dba6c006bedb4d0285fd9
Maxim <m.glushchenko@office.hoster.kz>
2025-09-09 17:13:03 +0500

    Fix typo

--- app/resume/resume_router.py
+++ app/resume/resume_router.py
@@ -1,14 +1,16 @@
 from fastapi import APIRouter
-from app.core.dependencies import CurrentUser, Session
+from app.core.dependencies import CurrentUser, SessionDep
 from app.resume.resume_shemas import ResumeSchema
 from app.db.crud import create_resume
 
 router = APIRouter(tags=["resume"], prefix="/resume")
 
 
-router.post("/")
+@router.post("/")
 def add_resume(
-    session: Session, resume: ResumeSchema, current_user: CurrentUser
+    session: SessionDep, resume: ResumeSchema, current_user: CurrentUser
 ) -> str:
-    create_resume(session=session, resume=resume, user=current_user)
+    create_resume(session=session, resume=resume, db_user=current_user)
     return "Resume added successfully"
+
+
